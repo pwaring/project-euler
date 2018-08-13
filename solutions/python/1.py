3b3a10654baa698dd162f17cdcf54b4f3c2b2776
Paul Waring <paul@xk7.net>
2018-08-13 19:43:17 +0100

    Rename sum as total

--- solutions/python/1.py
+++ solutions/python/1.py
@@ -1,9 +1,8 @@
 max_number = 1000
-sum = 0
+total = 0
 
 for x in range(0, max_number):
   if x % 3 == 0 or x % 5 == 0:
-    sum += x
+    total += x
 
-print("Sum of all multiples of 3 or 5 below " + str(max_number) + ": " + str(sum))
-
+print("Sum of all multiples of 3 or 5 below " + str(max_number) + ": " + str(total))
