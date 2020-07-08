60df9669ced341635fb61b6a9f8cbf4b2c4a5873
Paul Waring <paul@xk7.net>
2020-07-08 15:46:29 +0100

    Solution to problem 1 (Go)

---
+++ solutions/go/1/1.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var sum int = 0
+	
+	for num := 1; num < 1000; num++ {
+		if num % 3 == 0 || num % 5 == 0 {
+			sum += num
+		}
+	}
+
+	fmt.Println(sum)
+}
